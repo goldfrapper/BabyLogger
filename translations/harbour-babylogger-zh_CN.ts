385a8e689c7b348c398c8905f03b46e5bbdb0200
TylerTemp <tylertempdev@gmail.com>
2015-02-07 19:51:43 +0800

    add Chinese simplified translation

---
+++ translations/harbour-babylogger-zh_CN.ts
@@ -0,0 +1,297 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="19"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="39"/>
+        <source>Native SailfishOS application for monitoring/logging activity of a human baby.</source>
+        <translation>一个旗鱼系统原生应用，可以用来监控/记录人类宝宝的活动。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="48"/>
+        <source>Developer</source>
+        <translation>开发者</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="49"/>
+        <source>Goldfrapper &lt;goldfrapper@gmail.com&gt;</source>
+        <translation>Goldfrapper &lt;goldfrapper@gmail.com&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="54"/>
+        <source>GitHub Repository</source>
+        <translation>GitHub仓库</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="55"/>
+        <source>https://github.com/goldfrapper/BabyLogger/</source>
+        <translation>https://github.com/goldfrapper/BabyLogger/</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="60"/>
+        <source>Icon</source>
+        <translation>图标</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="61"/>
+        <source>Icon based on the artwork of Yazmin Alanis (http://thenounproject.com/yalanis/)</source>
+        <translation>基于Yazmin Alanis（http://thenounproject.com/yalanis/）的插图</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="67"/>
+        <source>Translations</source>
+        <translation>翻译</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="74"/>
+        <source>Licence</source>
+        <translation>许可证</translation>
+    </message>
+</context>
+<context>
+    <name>BabyModel</name>
+    <message>
+        <location filename="../qml/pages/BabyModel.qml" line="11"/>
+        <source>Breast milk</source>
+        <translation>母乳</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BabyModel.qml" line="11"/>
+        <source>Formula</source>
+        <translation>配方</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BabyModel.qml" line="11"/>
+        <source>Pureed food</source>
+        <translation>粥糊食物</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BabyModel.qml" line="13"/>
+        <source>Sleep stopped</source>
+        <translation>睡眠结束</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BabyModel.qml" line="14"/>
+        <source>Sleep started</source>
+        <translation>睡眠开始</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BabyModel.qml" line="15"/>
+        <source>Meal</source>
+        <translation>进餐</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BabyModel.qml" line="493"/>
+        <location filename="../qml/pages/BabyModel.qml" line="506"/>
+        <source>Update failed</source>
+        <translation>更新失败</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BabyModel.qml" line="494"/>
+        <source>The given time must not be in the future</source>
+        <translation>给定的时间不能在未来</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BabyModel.qml" line="507"/>
+        <source>You cannot update beyond the boundaries of adjoining sleep logs</source>
+        <translation>无法更新超出边界的相邻睡眠记录的内容</translation>
+    </message>
+</context>
+<context>
+    <name>Counter</name>
+    <message>
+        <location filename="../qml/pages/Counter.qml" line="87"/>
+        <source>since last meal</source>
+        <translation>从上次进餐开始</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Counter.qml" line="111"/>
+        <source>sleeping...</source>
+        <translation>正在睡觉…</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Counter.qml" line="111"/>
+        <source>awake...</source>
+        <translation>醒来…</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="47"/>
+        <source>You have not yet logged anything. Start by selecting &apos;Start sleep&apos; or &apos;Log meal&apos; from the pull-down menu.</source>
+        <translation>没有任何记录。从下拉菜单中选择 &apos;开始睡眠&apos; 或 &apos;记录进餐&apos; 以开始。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="106"/>
+        <source>total sleep</source>
+        <translation>睡眠总记录</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="128"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="134"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="140"/>
+        <source>Log Meal</source>
+        <translation>记录进餐</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="165"/>
+        <source>Stop sleep</source>
+        <translation>停止睡眠</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="165"/>
+        <source>Start sleep</source>
+        <translation>开始睡眠</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="170"/>
+        <source>Actions Menu</source>
+        <translation>行为菜单</translation>
+    </message>
+</context>
+<context>
+    <name>LogEntryItem</name>
+    <message>
+        <location filename="../qml/pages/LogEntryItem.qml" line="69"/>
+        <source>Sleeping</source>
+        <translation>睡觉</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LogEntryItem.qml" line="69"/>
+        <source>Awake</source>
+        <translation>醒来</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LogEntryItem.qml" line="110"/>
+        <source>Set time</source>
+        <translation>设置时间</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LogEntryItem.qml" line="127"/>
+        <source>Updating time</source>
+        <translation>更新时间</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LogEntryItem.qml" line="157"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LogEntryItem.qml" line="160"/>
+        <source>Deleting entry</source>
+        <translation>正在删除条目</translation>
+    </message>
+</context>
+<context>
+    <name>LogMealDialog</name>
+    <message>
+        <location filename="../qml/pages/LogMealDialog.qml" line="19"/>
+        <source>Add</source>
+        <translation>添加</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LogMealDialog.qml" line="26"/>
+        <source>Type</source>
+        <translation>类型</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LogMealDialog.qml" line="46"/>
+        <source>Quantity</source>
+        <translation>数量</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LogMealDialog.qml" line="65"/>
+        <source>Common choices:</source>
+        <translation>一般可选项：</translation>
+    </message>
+</context>
+<context>
+    <name>NotificationDialog</name>
+    <message>
+        <location filename="../qml/pages/NotificationDialog.qml" line="16"/>
+        <source>Back</source>
+        <translation>返回</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="21"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="25"/>
+        <source>Alerts and Timers</source>
+        <translation>提醒和记时</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="46"/>
+        <source>This value controls when the application should warn you that the baby has been awake for too long</source>
+        <translation>这个值用来提醒你宝宝醒着的时间太长了</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="67"/>
+        <source>This is the maximum time that should be allowed between meals. This greatly depends on the age of the baby</source>
+        <translation>进餐的最大时间间隔。很大程度上取决于宝宝的年龄</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="73"/>
+        <source>Application Reset</source>
+        <translation>重置应用</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="78"/>
+        <source>Clear all logs</source>
+        <translation>清除所有记录</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="91"/>
+        <source>Reset Application</source>
+        <translation>重置记录</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="92"/>
+        <source>This will remove all saved data. Are you shure this is what you want? There is no easy way to undo this</source>
+        <translation>即将移除所有保存的数据。恢复将会非常困难。你确定这样做吗？</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="30"/>
+        <source>Max. Awake Time</source>
+        <translation>最大值。清醒时间</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="51"/>
+        <source>Max. &apos;Between meals&apos; Time</source>
+        <translation>最大值。 &apos;进餐间隔&apos; 时间</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-babylogger</name>
+    <message>
+        <location filename="../qml/harbour-babylogger.qml" line="10"/>
+        <source>Baby Logger</source>
+        <translation>Baby Logger</translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-babylogger.qml" line="11"/>
+        <source>0.4.5</source>
+        <translation>0.4.5</translation>
+    </message>
+</context>
+</TS>
